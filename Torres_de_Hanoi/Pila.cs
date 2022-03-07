ae64a31674f19440321f15a5a08c04c212c477f6
Sairo1234 <alejandronogbel@gmail.com>
2022-03-07 15:26:25 +0100

    Primer commit

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -9,14 +9,11 @@ namespace Torres_de_Hanoi
     class Pila
     {
         public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
+        
+        public int Top { get; set; }        
+
         public List<Disco> Elementos { get; set; }
-        */
+       
 
         /* TODO: Implementar métodos */
         public Pila()
