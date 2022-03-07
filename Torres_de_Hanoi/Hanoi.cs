bc5149c93fc179ce0b82bb6b288df38950dc0df0
Sairo1234 <alejandronogbel@gmail.com>
2022-03-07 16:41:18 +0100

    Metodo iterativo

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -8,6 +8,8 @@ namespace Torres_de_Hanoi
 {
     class Hanoi
     {
+        public int m = 0;
+
         /*TODO: Implementar métodos*/
         public void mover_disco(Pila a, Pila b)
         {
@@ -15,17 +17,40 @@ namespace Torres_de_Hanoi
             {
                 Disco d = a.pop();
                 b.push(d);
+                m++;
             }
             else
             {
                 Disco d = b.pop();
                 a.push(d);
+                m++;
             }
         }
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
-            return 0;
+            int m = 0;
+
+            if (n % 2 == 0)
+            {
+                while (fin.Size != n)
+                {
+                    mover_disco(ini, fin);
+                    mover_disco(ini, aux);
+                    mover_disco(aux, fin);
+                }
+            }
+            else
+            {
+                while (fin.Size != n)
+                {
+                    mover_disco(ini, aux);
+                    mover_disco(ini, fin);
+                    mover_disco(aux, fin);
+                }
+            }
+
+            return m;
         }
 
     }
